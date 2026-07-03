0bfa512b46be4b94c38a19c9e5709bd8c074b9d8
JAVIER NICOLAS ANTONIO ESCANILLA SANCHEZ <ja.escanillas@duocuc.cl>
2026-07-03 15:41:02 -0400

    Add files via upload

--- Javier Escanilla/Evaluacion_4.py
+++ Javier Escanilla/Evaluacion_4.py
@@ -31,10 +31,21 @@ def agregar():
         libros[contador] = {
             'nombre': nombre,
             'copias': copias,
-            'prestamo': prestamo
+            'prestamo': prestamo,
+            'disponible': 'DISPONIBLE'
         }
         print(f"Libro agregado con ID {contador}")
         contador += 1
+
+def eliminar():
+    print("Eliminación de Libro")
+    b_nombre = input("Ingrese el nombre del libro a eliminar: ")
+    for i in libros:
+        if libros[i]['nombre'] == b_nombre:
+            libros[i] = None
+            break
+
+
 
 def validar_libro(n, c, p):
     if n.strip() == '':
@@ -56,7 +67,13 @@ def validador_op(op):
             raise ValueError("La opción debe estar entre 1 y 6")
     else:
         raise ValueError("Debe ingresar un número")
-    
+
+def validador_dispo():
+    for i in libros:
+        if libros[i]['copias'] == 0:
+            libros[i]['disponible'] == 'SIN COPIAS'
+        elif libros[i]['copias'] > 0:
+            libros[i]['disponible'] == 'DISPONIBLE'
 
 def buscar():
     print("Busqueda por nombre")
@@ -67,9 +84,10 @@ def buscar():
             print(f"Nombre: {libros[i]['nombre']}")
             print(f"Copias: {libros[i]['copias']}")
             print(f"Prestamo: {libros[i]['prestamo']}")
+            print(f"Disponibilidad: {libros[i]['disponible']}")
+            return True
+    return False
 
-def dispo():
-    pass
 
 def main():
     while True:
@@ -88,16 +106,37 @@ def main():
                     limpiar()
                     titulo()
                     print("*** Buscar Libro ***")
-                    buscar()
+                    if buscar():
+                        print("Volviendo...")
+                    else:
+                        print("=== Titulo no encontrado ===")
+                elif opcion == "3":
+                    limpiar()
+                    titulo()
+                    print("*** Eliminar Libro ***")
+                    eliminar()
+                elif opcion == "4":
+                    limpiar()
+                    titulo()
+                    validador_dispo()
+                    print("=== Disponibilidad Actualizada ===")
                 elif opcion == "5":
                     limpiar()
                     titulo()
                     print("*** Mostrar Libros ***")
-                    print(libros)
+                    print("=== Lista de Libros ===")
+                    if libros != None:
+                        for i in libros:
+                            print(f"\nNombre: {libros[i]['nombre']}")
+                            print(f"Copias: {libros[i]['copias']}")
+                            print(f"Prestamo: {libros[i]['prestamo']}")
+                            print(f"Disponibilidad: {libros[i]['disponible']}")
+                    else:
+                        print("=== No hay titulos disponible ===")
+                        
                 elif opcion == "6":
                     print("Saliendo...")
                     break
-
             input("\nPresione Enter para continuar...")
             limpiar()
 
